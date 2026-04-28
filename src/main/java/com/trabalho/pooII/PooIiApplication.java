dab1d0b3bfb4f934bc0f766b9d1dc7244cd2b0a3
Robert Lopes Terra <contatorobertlopes@gmail.com>
2026-04-27 21:37:34 -0300

    Arquivos iniciais do back-end

---
+++ src/main/java/com/trabalho/pooII/PooIiApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.pooII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/pooII/PooIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.pooII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
