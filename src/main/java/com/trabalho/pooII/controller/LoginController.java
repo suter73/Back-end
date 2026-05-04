b870fcc6466b642fb5447475d8d3cea3cd0d116a
Robert Lopes Terra <contatorobertlopes@gmail.com>
2026-05-04 19:10:47 -0300

    Adiciona endpoint para login

--- src/main/java/com/trabalho/pooII/controller/LoginController.java
+++ src/main/java/com/trabalho/pooII/controller/LoginController.java
@@ -1,10 +1,16 @@
 package com.trabalho.pooII.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.trabalho.pooII.dto.CadastroRequestDTO;
 import com.trabalho.pooII.dto.CadastroResponseDTO;
+import com.trabalho.pooII.dto.LoginRequestDTO;
+import com.trabalho.pooII.dto.LoginResponseDTO;
 import com.trabalho.pooII.services.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -16,5 +22,10 @@ public class LoginController {
     @PostMapping("/cadastro")
     public CadastroResponseDTO cadastrar(@RequestBody CadastroRequestDTO dto) {
         return loginService.cadastrar(dto);
+    }
+
+    @PostMapping()
+    public LoginResponseDTO logar(@RequestBody LoginRequestDTO dto) {
+        return loginService.logar(dto);
     }
 }
