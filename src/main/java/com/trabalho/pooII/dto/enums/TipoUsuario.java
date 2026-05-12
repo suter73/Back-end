844c741110c042ab30d84d64df92a7ad82ae8c5a
Robert Lopes Terra <contatorobertlopes@gmail.com>
2026-05-11 22:02:53 -0300

    Corrige códigos de pacientes e médicos

--- src/main/java/com/trabalho/pooII/dto/enums/TipoUsuario.java
+++ src/main/java/com/trabalho/pooII/dto/enums/TipoUsuario.java
@@ -2,12 +2,16 @@ package com.trabalho.pooII.dto.enums;
 
 public enum TipoUsuario {
 
-    MEDICO(1),
-    PACIENTE(2);
+    PACIENTE(1),
+    MEDICO(2);
 
     private final Integer codigo;
 
     TipoUsuario(Integer codigo) {
         this.codigo = codigo;
+    }
+
+    public Integer getCodigo(){
+        return codigo;
     }
 }
